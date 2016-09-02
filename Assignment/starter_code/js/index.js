16317159855d906a3e12b8f0858c990cccf05b0f
rtaykowski <rtaykowski@Roman-Taykowskis-MacBook-Pro.local>
2016-09-01 22:18:31 -0700

    progress 9.1.16

--- Assignment/starter_code/js/index.js
+++ Assignment/starter_code/js/index.js
@@ -1,14 +1,13 @@
 //define variables
 
-var NYC = city("New York", "New York City", "NYC");
+/*var NYC = city("New York", "New York City", "NYC");
 var SF = city("San Francisco", "SF", "Bay Area");
 var LA = city("Los Angeles", "LA", "LAX");
 var ATX = city("Austin", "ATX");
 var SYD = city("Sydney", "SYD");
+*/
 
-//define function
-
-function city(one, two, three){
+/*function changeBackground(one, two, three){
 	if(one == "New York" || two == "New York City" || three == "NYC"){
 		console.log("change bkgd img to NYC")
 	}
@@ -32,10 +31,70 @@ function city(one, two, three){
 	console.log(one, two, three);
 	return one+", "+two+", "+three;
 
-}
+}*/
+
 
 //if user inputs a name of city
 
-$("#city-type").val("");
+//when user submits form retrieve value of input
+$(document).ready(function(){
+
+$("#submit-btn").click(showPhoto);
+
+function showPhoto(){
+	event.preventDefault();
+	var city = $("#city-type").val();
+
+	changeBackground(city); 
+
+} 
+
+//change background by using a function
+
+function changeBackground(city){
+	if(city == "New York" || city == "New York City" || city == "NYC"){
+		console.log("change bkgd img to NYC");
+		$("body").attr("class" , "nyc");
+	}
+
+	else if (city == "San Francisco" || city == "SF" || city == "Bay Area"){
+		console.log("change bkgd img to SF");
+		$("body").attr( "class" , "sf");
+		/*$("body").removeClass("nyc");
+		$("body").removeClass("la");
+		$("body").removeClass("austin");
+		$("body").removeClass("sydney");*/
+	}
+
+	else if (city == "Los Angeles" || city == "LA" || city == "LAX"){
+		console.log("change bkgd img to LA")
+		$("body").attr("class" , "la");
+		/*$("body").removeClass("nyc");
+		$("body").removeClass("sf");
+		$("body").removeClass("austin");
+		$("body").removeClass("sydney");*/
+	}
+
+	else if (city == "Austin" || city == "ATX"){
+		console.log("change bkgd img to ATX")
+		$("body").attr("class" , "austin");
+		/*$("body").removeClass("nyc");
+		$("body").removeClass("sf");
+		$("body").removeClass("la");
+		$("body").removeClass("sydney");*/
+	}
+
+	else if (city == "Sydney" || city == "SYD"){
+		console.log("change bkgd img to SYD")
+		$("body").attr("class" , "sydney");
+		/*$("body").removeClass("nyc");
+		$("body").removeClass("sf");
+		$("body").removeClass("austin");
+		$("body").removeClass("la");*/
+	}
+
+}
+
+});
 
 
